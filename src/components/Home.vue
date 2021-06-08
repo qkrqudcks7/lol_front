9459ab5f16837df2e4a4b5c634220ccb82eefc2a
qkrqudcks7 <66015002+qkrqudcks7@users.noreply.github.com>
2021-06-08 23:15:52 +0900

    cors 설정

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,8 +3,21 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: "Home"
+  name: "Home",
+  data () {
+    return {
+
+    }
+  },
+  created () {
+    axios.get("/lol/summoner/v4/summoners/by-name/hide on bush?api_key=RGAPI-704f1a4b-2987-4974-9e2a-07b459a0c3ea")
+    .then(response => {
+      console.log(response.data)
+    })
+  }
 }
 </script>
 
---
+++ vue.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    devServer: {
+        proxy:'https://kr.api.riotgames.com'
+    }
+};
