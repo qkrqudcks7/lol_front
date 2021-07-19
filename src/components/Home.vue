321bb2eca184edb760cfb7482d8345228c5f87b4
qkrqudcks7 <66015002+qkrqudcks7@users.noreply.github.com>
2021-07-19 14:08:17 +0900

    데이터 처리

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -22,22 +22,23 @@
 
   <div>
     <h3>최근 10게임</h3>
-    <div v-for="(i,index) in match" :key="index">
-      <div>챔피언: {{findCharacterId(i.champion)}}</div>
-      <div>라인: {{i.lane}}</div>
-      <div>큐: {{i.queue}}</div>
-      <div>역할: {{i.role}}</div>
-      <div>시즌: {{i.season}}</div>
-      <div>시간: {{$moment(i.timestamp).format('YYYY-MM-DD HH:mm:SS')}}</div>
-      <div>--------------------------------</div>
-    </div>
     <div v-for="(j,index) in detail" :key="'a'+index">
       <div>{{j[0]}}</div>
       <div>{{j[1]}}</div>
-      <div>{{j[2]}}</div>
-      <div>킬: {{j[3]}}</div>
-      <div>데스: {{j[4]}}</div>
-      <div>어시: {{j[5]}}</div>
+      <img :src="j[3]">
+      <img :src="j[5]">
+      <div>{{j[6]}} / {{j[7]}} / {{j[8]}}</div>
+      <div>평점 : {{ ((j[6] + j[8]) / j[7]).toFixed(2)}}</div>
+      <div>레벨 : {{j[9]}}</div>
+      <div>cs : {{j[10]}}</div>
+      <img :src="j[11]">
+      <img :src="j[12]">
+      <img :src="j[13]">
+      <img :src="j[14]">
+      <img :src="j[16]">
+      <img :src="j[17]">
+      <img :src="j[15]">
+      <div>--------------------------------------------------------------------</div>
     </div>
   </div>
   </div>
@@ -45,6 +46,8 @@
 
 <script>
 import championFile from '../lol_data/champion.json'
+import spellFile from '../lol_data/summoner.json'
+import itemFile from '../lol_data/item.json'
 import lolAPI from '../API/lolAPI'
 export default {
   name: 'Home',
@@ -80,6 +83,7 @@ export default {
           for (let b of this.match) {
             await lolAPI.find_detail_match(b.gameId).then(response => {
               let array = []
+              array.push(this.findCharacterId(b.champion))
               let data = response.data
               let id = ''
               for (let i = 1; i < data.participantIdentities.length; i++) {
@@ -90,14 +94,52 @@ export default {
               }
               for (let i = 0; i < 2; i++) {
                 if (data.teams[i].teamId === data.participants[id].teamId) {
-                  array.push(data.teams[i].win)
+                  if (data.teams[i].win === 'Win') {
+                    array.push('승리')
+                  } else {
+                    array.push('패배')
+                  }
                 }
               }
-              array.push(data.participants[id - 1].spell1Id)
-              array.push(data.participants[id - 1].spell2Id)
+              let file = spellFile.data
+              for (let i in file) {
+                if (file[i].key === String(data.participants[id - 1].spell1Id)) {
+                  array.push(file[i].name)
+                  array.push('static/spell/' + file[i].id + '.png')
+                  break
+                }
+              }
+              for (let i in file) {
+                if (file[i].key === String(data.participants[id - 1].spell2Id)) {
+                  array.push(file[i].name)
+                  array.push('static/spell/' + file[i].id + '.png')
+                  break
+                }
+              }
               array.push(data.participants[id - 1].stats.kills)
               array.push(data.participants[id - 1].stats.deaths)
               array.push(data.participants[id - 1].stats.assists)
+              array.push(data.participants[id - 1].stats.champLevel)
+              array.push(data.participants[id - 1].stats.totalMinionsKilled)
+              let Ifile = itemFile.data
+              for (let i in Ifile) {
+                if (i === String(data.participants[id - 1].stats.item0)) {
+                  array.push('static/item/' + Ifile[i].image.full)
+                } else if (i === String(data.participants[id - 1].stats.item1)) {
+                  array.push('static/item/' + Ifile[i].image.full)
+                } else if (i === String(data.participants[id - 1].stats.item2)) {
+                  array.push('static/item/' + Ifile[i].image.full)
+                } else if (i === String(data.participants[id - 1].stats.item3)) {
+                  array.push('static/item/' + Ifile[i].image.full)
+                } else if (i === String(data.participants[id - 1].stats.item4)) {
+                  array.push('static/item/' + Ifile[i].image.full)
+                } else if (i === String(data.participants[id - 1].stats.item5)) {
+                  array.push('static/item/' + Ifile[i].image.full)
+                } else if (i === String(data.participants[id - 1].stats.item6)) {
+                  array.push('static/item/' + Ifile[i].image.full)
+                }
+              }
+
               this.detail.push(array)
             })
           }
@@ -129,32 +171,6 @@ export default {
           console.log(error.response)
           this.message = '찾을 수 없는 사용자입니다.'
         }
-      })
-    },
-    findMatchDetail (matchId) {
-      return lolAPI.find_detail_match(matchId).then(response => {
-        let array = []
-        let data = response.data
-        console.log(data)
-        let id = ''
-        for (let i = 1; i < data.participantIdentities.length; i++) {
-          if (data.participantIdentities[i].player.summonerName === this.name) {
-            id = data.participantIdentities[i].participantId
-            break
-          }
-        }
-        for (let i = 0; i < 2; i++) {
-          if (data.teams[i].teamId === data.participants[id].teamId) {
-            array.push(data.teams[i].win)
-          }
-        }
-        array.push(data.participants[id - 1].spell1Id)
-        array.push(data.participants[id - 1].spell2Id)
-        array.push(data.participants[id - 1].stats.kills)
-        array.push(data.participants[id - 1].stats.deaths)
-        array.push(data.participants[id - 1].stats.assists)
-        console.log(array)
-        return array
       })
     }
   }
