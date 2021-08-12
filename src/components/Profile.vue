808e7b59099a4ead7fbef96377dd5561f3817d96
qkrqudcks7 <66015002+qkrqudcks7@users.noreply.github.com>
2021-08-12 17:06:34 +0900

    내정보창 ui 추가

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,10 +1,29 @@
 <template>
-  <div class="container">
+  <v-row justify="center">
     <header class="jumbotron">
-      <h3>username : {{content.username}}</h3>
-      <h3>email : {{content.email}}</h3>
-    </header>
-  </div>
+      <h1>{{content.username}}님의 개인정보</h1>
+    </header><br><br><br>
+    <v-expansion-panels popout>
+      <v-expansion-panel>
+        <v-expansion-panel-header>ID 확인하기</v-expansion-panel-header>
+        <v-expansion-panel-content>
+          {{content.username}}님의 Id는 <strong>{{content.email}}</strong>입니다.
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+      <v-expansion-panel>
+        <v-expansion-panel-header>권한 확인하기</v-expansion-panel-header>
+        <v-expansion-panel-content>
+          {{content.username}}님은 <strong>{{findAuthority}}</strong>입니다.
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+      <v-expansion-panel>
+        <v-expansion-panel-header>여기 뭐넣지</v-expansion-panel-header>
+        <v-expansion-panel-content>
+          ....
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
+  </v-row>
 </template>
 
 <script>
@@ -21,9 +40,21 @@ export default {
     AuthService.getUserContent()
       .then(response => {
         this.content = response.data
+        console.log(this.content)
       }, error => {
         this.content = error.response.data.message
       })
+  },
+  computed: {
+    findAuthority () {
+      if (this.content.authorities[0].authority === 'ROLE_USER') {
+        return '일반 사용자'
+      } else if (this.content.authorities[0].authority === 'ROLE_ADMIN') {
+        return '관리자'
+      } else {
+        return '우수 사용자'
+      }
+    }
   }
 }
 </script>
